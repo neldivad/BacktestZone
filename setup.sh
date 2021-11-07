664fc8d4ffa3eb0077793e9d07eb10937476cd50
Nikhil Adithyan <69444301+Nikhil-Adithyan@users.noreply.github.com>
2021-11-07 15:14:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 7 10:47:19 2021
-@author: Nikhil Adithyan
-"""
-
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
-headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
